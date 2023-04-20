a69b95d43a1d648f50543d7feaf08849e6caa5d2
sandeepsimsaw <sandeep.saini@simsaw.com>
2023-04-20 16:57:28 +0530

    work on template

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,80 @@
+<!-- component -->
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img
+      class="mx-auto h-10 w-auto"
+      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+      alt="Your Company"
+    />
+    <h2
+      class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+    >
+      Sign in to your account
+    </h2>
+  </div>
+  <div class="bg-grey-lighter min-h-screen flex flex-col">
+    <div
+      class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+    >
+      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+        <input
+          type="text"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="fullname"
+          placeholder="Full Name"
+        />
+
+        <input
+          type="text"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="email"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="password"
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="confirm_password"
+          placeholder="Confirm Password"
+        />
+
+        <button
+          type="submit"
+          class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
+          >Create Account</button
+        >
+
+        <div class="text-center text-sm text-grey-dark mt-4">
+          By signing up, you agree to the
+          <a
+            class="no-underline border-b border-grey-dark text-grey-dark"
+            href="#"
+          >
+            Terms of Service
+          </a>
+          and
+          <a
+            class="no-underline border-b border-grey-dark text-grey-dark"
+            href="#"
+          >
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+
+      <div class="text-grey-dark mt-6">
+        Already have an account?
+        <a class="no-underline border-b border-blue text-blue" href="../login/">
+          Log in
+        </a>.
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/student/+layout.svelte
@@ -0,0 +1,39 @@
+
+<script lang='ts'>
+	import { onMount } from 'svelte';
+ 
+	import SideBar from '$lib/sidebar.svelte';
+	import SideBarToggle from '$lib/SideBarToggle.svelte';
+	import PageHeader from '$lib/PageHeader.svelte';
+	let isOpen=false;
+	 function handleToggleMenu(){
+        //isOpen=!isOpen;
+	 }
+</script>
+
+<div class="bg-gray-50 dark:bg-slate-900"> 
+	<!-- ========== MAIN CONTENT ========== -->
+	<div class="drawer  drawer-mobile">
+		<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+		<div class="drawer-content">
+		  <!-- Page content here -->
+		  <SideBarToggle on:toggle={handleToggleMenu}  />
+		</div> 
+		<div class="drawer-side">
+		  <label for="my-drawer" class="drawer-overlay"></label>
+		   <!-- Add SideBar Component Here-->
+			<SideBar {isOpen } />
+			<!-- SideBar Component-->
+		</div>
+          <!-- Content -->
+          <div class="w-full pt-10 px-4 sm:px-6 md:px-8 lg:pl-72">
+            <PageHeader />
+            <slot></slot>
+                
+            </div>
+        <!-- End Content -->
+    </div> 
+	
+  
+	<!-- ========== END MAIN CONTENT ========== -->
+</div>
